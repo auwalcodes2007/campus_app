cea056787a02ebf8e72be286873532bfc6c97c7b
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-15 15:07:58 +0100

    Move auth url path from auth routes to app init for easier debugging

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
+# Configurations
+
 def create_app():
     app = Flask(__name__)
-    # Configurations
 
     # Initialize extensions
 
@@ -10,6 +11,6 @@ def create_app():
     from .main.routes import main_bp
     from .auth.routes import auth_bp
     app.register_blueprint(main_bp)
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp,  url_prefix='/auth')
     
     return app
