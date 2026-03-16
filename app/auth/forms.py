5bd71cf5fdd8578d4c7196134d88c5d230f285ed
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-16 18:15:58 +0100

    feat: add confirm password to registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    email = StringField("Email", validators=[DataRequired(), Email(message='Invalid email address')])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message='Password must be at least 6 characters long')])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), Length(min=6), EqualTo('password', message='Passwords must match')])
     submit = SubmitField("Register")
 
 class LoginForm(FlaskForm):
