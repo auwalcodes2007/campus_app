2e88deb9253b463defb147113fac807d517131bc
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-17 17:47:58 +0100

    - Built Register/Login routes with dynamic 'next' parameter handling

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from .forms import RegisterForm, LoginForm
 from ..models import db, User
 from .. import bcrypt
+from urllib.parse import urlparse
 
 
 auth_bp = Blueprint('auth' , __name__, template_folder="templates")
@@ -20,7 +21,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        return redirect(url_for('main.home'))
+        return redirect(url_for('main.dashboard'))
     return render_template("auth/register.html", form=form)
 
 @auth_bp.route("/login", methods=["GET", "POST"])
@@ -30,12 +31,15 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         # Handle login logic here (e.g., verify user, check password, etc.)
-        password = form.password.data
         result = db.session.execute(db.select(User).where(User.email == form.email.data))
         user = result.scalar()
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            return redirect(url_for('auth.dashboard'))
+            next_page = request.args.get('next')
+            print(next_page)
+            if not next_page or urlparse(next_page).netloc != '':
+                next_page = url_for('main.dashboard')
+            return redirect(next_page)
         flash('Invalid username or password. Please try again.', 'error')
     return render_template("auth/login.html", form=form)
 
