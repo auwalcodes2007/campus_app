600bea4ffcf700bc4051a5463c5bf3f2179b5e81
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-22 21:10:06 +0100

    Create course form

--- app/courses/forms.py
+++ app/courses/forms.py
@@ -4,4 +4,8 @@ from wtforms.validators import DataRequired, NumberRange
 
 
 class CourseForm(FlaskForm):
-    pass
+    course_code = StringField("Course Code", validators=[DataRequired()])
+    course_name = StringField("Course Name", validators=[DataRequired()])
+    credit_units = IntegerField("Credit Units", validators=[DataRequired(), NumberRange(min=0)])
+    grade_points = FloatField("Grade Points", validators=[NumberRange(min=0, max=5)])
+    semester = StringField("Semester", validators=[DataRequired()])
