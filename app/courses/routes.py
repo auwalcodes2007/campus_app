c884ab6737146920022fa6506a9579fefe4ed4f1
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-24 21:50:06 +0100

    create add course page

--- app/courses/routes.py
+++ app/courses/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
+from .forms import CourseForm
 
 courses_bp = Blueprint("courses", __name__, template_folder="templates")
 
@@ -11,7 +12,11 @@ def courses():
 @courses_bp.route("/add", methods=["GET", "POST"])
 @login_required
 def add_course():
-    pass
+    form = CourseForm()
+    if form.validate_on_submit():
+        pass
+    return render_template("courses/add_course.html", form=form)
+
 
 @courses_bp.route("/<int:id>/edit", methods=["GET", "POST"])
 @login_required
